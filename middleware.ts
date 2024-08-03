6331806f288009758b0e1406b53c8d816bc79af5
murtaza <muhammadmurtazanasir90@gmail.com>
2024-08-03 05:25:54 +0300

    f1

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,24 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware
+// Define your public routes
+const publicRoutes = [
+  '/',
+  '/api/webhooks/clerk',
+  '/api/webhooks/stripe',
+];
+
+// Create a regex pattern that matches all routes except the public ones
+const excludePublicRoutesPattern = `^(${publicRoutes.join('|')})$`;
+
+export default clerkMiddleware({
+  // Middleware options here if needed
+});
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Exclude public routes
+    `!/${excludePublicRoutesPattern}`,
+    "/((?!.+\\.[\\w]+$|_next).*)", // Matches all routes except file extensions and _next paths
+    "/(api|trpc)(.*)"              // Matches API and trpc routes
+  ],
 };
